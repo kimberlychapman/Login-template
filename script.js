a85e098911d2c6e462ccce43ab6df22ec869b97e
kimberlychapman <63875440+kimberlychapman@users.noreply.github.com>
2021-07-17 09:39:56 -0500

    added files

---
+++ script.js
@@ -0,0 +1,99 @@
+function bigPic(){
+    document.getElementById("avatar").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar").style.border="4px solid white";
+}
+
+function resetPic(){
+    document.getElementById("avatar").style.boxShadow="none";
+    document.getElementById("avatar").style.border="solid 1px white";
+}
+
+function bigPic2(){
+    document.getElementById("avatar2").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar2").style.border="4px solid white";
+}
+
+function resetPic2(){
+    document.getElementById("avatar2").style.boxShadow="none";
+    document.getElementById("avatar2").style.border="solid 1px white";
+}
+
+function bigPic3(){
+    document.getElementById("avatar3").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar3").style.border="4px solid white";
+}
+
+function resetPic3(){
+    document.getElementById("avatar3").style.boxShadow="none";
+    document.getElementById("avatar3").style.border="solid 1px white";
+}
+
+function bigPic4(){
+    document.getElementById("avatar4").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar4").style.border="4px solid white";
+}
+
+function resetPic4(){
+    document.getElementById("avatar4").style.boxShadow="none";
+    document.getElementById("avatar4").style.border="solid 1px white";
+}
+
+function bigPic5(){
+    document.getElementById("avatar5").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar5").style.border="4px solid white";
+}
+
+function resetPic5(){
+    document.getElementById("avatar5").style.boxShadow="none";
+    document.getElementById("avatar5").style.border="solid 1px white";
+}
+
+function bigPic6(){
+    document.getElementById("avatar6").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar6").style.border="4px solid white";
+}
+
+function resetPic6(){
+    document.getElementById("avatar6").style.boxShadow="none";
+    document.getElementById("avatar6").style.border="solid 1px white";
+}
+
+function bigPic7(){
+    document.getElementById("avatar7").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar7").style.border="4px solid white";
+}
+
+function resetPic7(){
+    document.getElementById("avatar7").style.boxShadow="none";
+    document.getElementById("avatar7").style.border="solid 1px white";
+}
+
+function bigPic8(){
+    document.getElementById("avatar8").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar8").style.border="4px solid white";
+}
+
+function resetPic8(){
+    document.getElementById("avatar8").style.boxShadow="none";
+    document.getElementById("avatar8").style.border="solid 1px white";
+}
+
+function bigPic9(){
+    document.getElementById("avatar9").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar9").style.border="4px solid white";
+}
+
+function resetPic9(){
+    document.getElementById("avatar9").style.boxShadow="none";
+    document.getElementById("avatar9").style.border="solid 1px white";
+}
+
+function bigPic10(){
+    document.getElementById("avatar10").style.boxShadow="0 0 10px 0  white inset, 0 0 20px 2px white";
+    document.getElementById("avatar10").style.border="4px solid white";
+}
+
+function resetPic10(){
+    document.getElementById("avatar10").style.boxShadow="none";
+    document.getElementById("avatar10").style.border="solid 1px white";
+}
